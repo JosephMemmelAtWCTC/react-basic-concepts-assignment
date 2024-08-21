d9f3087d2cd16948fc1716dbd023869aa8e9e4c5
42null <averyparanoiduser@gmail.com>
2024-08-21 09:28:20 -0500

    React More Basic Concepts - Basic Level

--- src/App.js
+++ src/App.js
@@ -4,15 +4,40 @@ import './App.css';
 import Country from './components/Country'
 
 class App extends Component {
-  state = { };
+  state = {
+    countries: [
+      { id: 1, name: 'United States', gold: 2 },
+      { id: 2, name: 'China', gold: 3 },
+      { id: 3, name: 'Germany', gold: 0 },
+    ]
+  };
+
+  handleGoldUpdate = (countryId, goldChange) => {
+    console.log(countryId, goldChange);
+    const updateCountryIndex = this.state.countries.map(c => c.id).indexOf(countryId);
+    const countriesCopy = this.state.countries;
+    countriesCopy[updateCountryIndex].gold += goldChange;
+
+    if(countriesCopy[updateCountryIndex].gold < 0){
+      countriesCopy[updateCountryIndex].gold = 0;
+    }
+
+    this.setState({countries: countriesCopy})
+  }
+
   render() {
     return (
       <div className='App'>
         <header className='App-header'>
           Country Gold Metal Counter
         </header>
-        <Country countryName='test'>
-        </Country>
+        {this.state.countries.map(country =>
+          <Country
+            key={country.id}
+            country={country}
+            onGoldUpdate={this.handleGoldUpdate}
+          />
+        )}
       </div>
     )
   }
